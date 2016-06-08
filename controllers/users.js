aa9837836c26e2aaf5edf9285bfea8b7c87d601f
Sven Heinrich <sven.heinrich@mimacom.com>
2016-06-08 16:02:03 +0200

    another bugfix (Email)

--- controllers/users.js
+++ controllers/users.js
@@ -195,7 +195,7 @@ exports.logout = function (req, res, next) {
 exports.token = function (req, res, next) {
 
     var user = new User();
-    user.email = 'user_' + (Math.random() * 999999999);
+    user.email = 'user_' + (Math.random() * 99999999) + '@svendroid.com';
     user.username = user.email;
     user.provider = 'local';
     user.password = 'svendroid';
